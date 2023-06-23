33a96c5650d907c4211cc5f8d42ae30b6af8a535
Siziwe Fortunate Sithole <fortunatesithole@icloud.com>
2023-06-23 15:02:18 +0200

    Adding an API script which will call the API directly

--- zutari-unity-2023-siziwe/Assets/Scripts/WeatherAPI.cs
+++ zutari-unity-2023-siziwe/Assets/Scripts/WeatherAPI.cs
@@ -1,53 +1,66 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 
 public class WeatherAPI : MonoBehaviour
 {
-    public string apiKey = "51b4da4c515478f4f5de608451806962";
-    public string[] cityNames = { "Pretoria", "City2", "City3" }; // Add the capital city names of each province in South Africa
-
     public Text cityNameText;
     public Text temperatureText;
     public Text descriptionText;
 
-    private void IEnumerator Start()
+    private string apiKey = "51b4da4c515478f4f5de608451806962";
+    private List<string> capitalCities = new List<string> { "Johannesburg", "Cape Town", "Pretoria", "Durban", "Bloemfontein", "Nelspruit", "Kimberley", "Mahikeng", "Polokwane" };
+    private Dictionary<string, WeatherData> weatherDataMap = new Dictionary<string, WeatherData>();
+
+    private void Start()
     {
-        foreach (string city in cityNames)
+        StartCoroutine(GetWeatherDataForCities());
+    }
+
+    private IEnumerator GetWeatherDataForCities()
+    {
+        foreach (string city in capitalCities)
         {
-            StartCoroutine(GetWeatherData(city));
+            yield return StartCoroutine(GetWeatherData(city));
+        }
+
+        // Display weather data in the desired order
+        foreach (string city in capitalCities)
+        {
+            WeatherData weatherData = weatherDataMap[city];
+            UpdateWeatherUI(weatherData);
+            //yield return new WaitForSeconds(0.5f); // Optional delay between displaying each city's weather data
         }
     }
 
-    IEnumerator GetWeatherData(string city)
+    private IEnumerator GetWeatherData(string city)
     {
-        string url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",za&units=metric&appid=" + "51b4da4c515478f4f5de608451806962";
-        using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
-        {
-            yield return webRequest.SendWebRequest();
+        string url = $"http://api.openweathermap.org/data/2.5/weather?q={city},za&units=metric&appid={apiKey}";
 
-            if (webRequest.result != UnityWebRequest.Result.Success)
-            {
-                Debug.Log("Error: " + webRequest.error);
-            }
-            else
-            {
-                WeatherData weatherData = JsonUtility.FromJson<WeatherData>(webRequest.downloadHandler.text);
-                DisplayWeatherData(weatherData);
-            }
+        UnityWebRequest request = UnityWebRequest.Get(url);
+        yield return request.SendWebRequest();
+
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogError("Weather API request failed: " + request.error);
+        }
+        else
+        {
+            WeatherData weatherData = JsonUtility.FromJson<WeatherData>(request.downloadHandler.text);
+            weatherDataMap.Add(city, weatherData);
         }
     }
 
-    void DisplayWeatherData(WeatherData weatherData)
+    private void UpdateWeatherUI(WeatherData weatherData)
     {
-        cityNameText.text = weatherData.name;
-        temperatureText.text = weatherData.main.temp.ToString() + "°C";
-        descriptionText.text = weatherData.weather[0].description;
+        cityNameText.text += "\n" + weatherData.name;
+        temperatureText.text += "\n" + weatherData.main.temp.ToString("0") + "°C";
+        descriptionText.text += "\n" + weatherData.weather[0].description;
     }
 }
-
 [System.Serializable]
 public class WeatherData
 {
@@ -67,3 +80,6 @@ public class Weather
 {
     public string description;
 }
+
+
+
